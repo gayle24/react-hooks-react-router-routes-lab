13ab1aefe74bbcb388faec17b6e3fbe235170dd6
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-08-07 01:38:03 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,54 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
-}
+  const style = {
+    color: "white",
+    marginRight: "10px",
+    background: "purple"
+  }
+  return (
+    <div className="navbar">
+     <NavLink
+          to="/"
+          exact
+          style={style}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Home
+      </NavLink>
+      <NavLink
+          to="/movies"
+          exact
+          style={style}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Movies
+      </NavLink> 
+      <NavLink
+          to="/directors"
+          exact
+          style={style}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+         Directors
+      </NavLink>  
+      <NavLink
+          to="/actors"
+          exact
+          style={style}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+         Actors
+      </NavLink> 
+  </div>
+)}
 
 export default NavBar;
